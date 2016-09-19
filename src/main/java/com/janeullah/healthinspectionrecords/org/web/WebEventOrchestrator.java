55fd6e02ab1aa285b18a918fd301fa859b6f933f
Jane Ullah <janeullah@gmail.com>
2016-09-18 21:31:48 -0600

    commenting main method

--- src/main/java/com/janeullah/healthinspectionrecords/org/web/WebEventOrchestrator.java
+++ src/main/java/com/janeullah/healthinspectionrecords/org/web/WebEventOrchestrator.java
@@ -39,8 +39,8 @@ public class WebEventOrchestrator {
         return result;
     }
 
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         WebEventOrchestrator orch = new WebEventOrchestrator();
         orch.run();
-    }
+    }*/
 }
