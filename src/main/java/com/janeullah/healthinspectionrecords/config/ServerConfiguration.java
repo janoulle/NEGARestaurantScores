491e4a3530633f3892fff63f8f49c30d5cc1a5ca
Jane Ullah <janeullah@gmail.com>
2018-03-31 19:06:59 -0600

    added port and context path (two ways: java vs props)

---
+++ src/main/java/com/janeullah/healthinspectionrecords/config/ServerConfiguration.java
@@ -0,0 +1,11 @@
+package com.janeullah.healthinspectionrecords.config;
+
+/*@Component
+public class ServerConfiguration implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8080);
+        container.setContextPath("/restaurantscores");
+    }
+}*/
