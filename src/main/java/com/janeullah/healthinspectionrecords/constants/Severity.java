d9ee2c49b2c792d274550fadbf5da08e7f6b9ca0
Jane Ullah <janeullah@gmail.com>
2018-07-23 19:58:31 -0600

    experimenting with multi module

--- src/main/java/com/janeullah/healthinspectionrecords/constants/Severity.java
+++
@@ -1,9 +0,0 @@
-package com.janeullah.healthinspectionrecords.constants;
-
-/** Author: Jane Ullah Date: 9/17/2016 */
-public enum Severity {
-  LOW,
-  MEDIUM,
-  NONCRITICAL,
-  CRITICAL
-}
--- src/main/java/com/janeullah/healthinspectionrecords/exceptions/WebPageProcessAsyncException.java
+++
@@ -1,9 +0,0 @@
-package com.janeullah.healthinspectionrecords.exceptions;
-
-public class WebPageProcessAsyncException extends Exception {
-
-    public WebPageProcessAsyncException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-}
