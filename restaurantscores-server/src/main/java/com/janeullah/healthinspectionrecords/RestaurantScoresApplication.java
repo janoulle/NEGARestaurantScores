67b891388345598ca6d9693cc78daa1bd0e1336f
Jane Ullah <janeullah@gmail.com>
2018-07-25 20:51:55 -0600

    removing unused configuration code, cleaning up the tasks submodule

--- restaurantscores-server/src/main/java/com/janeullah/healthinspectionrecords/RestaurantScoresApplication.java
+++ restaurantscores-server/src/main/java/com/janeullah/healthinspectionrecords/RestaurantScoresApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
 
 @Slf4j
-@EnableScheduling
 @EnableCaching
 @SpringBootApplication(scanBasePackages = {"com.janeullah.healthinspectionrecords"})
 public class RestaurantScoresApplication {
---
+++ restaurantscores-task/src/main/java/com/janeullah/healthinspectionrecords/TaskApplication.java
@@ -0,0 +1,14 @@
+package com.janeullah.healthinspectionrecords;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+}
